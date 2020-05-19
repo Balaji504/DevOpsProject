0b5fd1af60e8e1af5891c97bb671c1558f584483
Parasuram Yerramsetty <parasuramyerramsetty@gmail.com>
2020-05-19 23:22:26 +0530

    terraform script updated
    terraform script updated

--- code/terraform/scripts/sample script/main.tf
+++ code/terraform/scripts/sample script/main.tf
@@ -1,18 +1,45 @@
 provider "aws" {
 profile    = "default"
 region     = "us-east-2"
-access_key = "XXX"
-secret_key = "XXXX"
+access_key = "XXXXX"
+secret_key = "XXXXXX"
 }
 
-
-
 resource "aws_instance" "example" {
-  ami  = "XXX"
+  ami  = "XXXXX"
   instance_type = "t2.micro"
-  key_name = "XXX"
+  key_name = "tracrat-dev"
   tags = {
-      Name = "XXX"
+      Name = "terraform-server"
    }
  }
+ 
+ variable "aws_vpc" {
+   default ="XXXXX"
+ }
+ 
+ resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${var.aws_vpc}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 
--- code/terraform/scripts/script2/main.tf
+++ code/terraform/scripts/script2/main.tf
@@ -7,3 +7,9 @@ resource "local_file" "testfile" {
     content     = "Content = ${var.sample}\n"
     filename = "/terraform_dir/test"
 }
+
+variable "aws_vpc.main.id" {
+   default ="XXXX"
+}
+
+vpc-e624f68d
